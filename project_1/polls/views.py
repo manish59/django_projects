24f7ae057a4b51ef3ed094edc73fd16f46f8cf18
Manish <manishkumar.bobbili3@gmail.com>
2017-10-24 01:58:57 -0400

    https://github.com/manish59/django_projects

--- project_1/polls/views.py
+++ project_1/polls/views.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request)
+def index(request):
     return HttpResponse("Hello World")
